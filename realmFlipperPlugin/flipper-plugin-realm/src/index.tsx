d65e3503f9a05fa3772dfda78feaa1142abb673e
lastnameswayne <swaynedev@gmail.com>
2022-07-13 14:18:30 +0200

    Added tabs to the plugin for different viewModes

--- realmFlipperPlugin/flipper-plugin-realm/src/index.tsx
+++ realmFlipperPlugin/flipper-plugin-realm/src/index.tsx
@@ -1,11 +1,17 @@
+import {
+  ConsoleSqlOutlined,
+  DatabaseOutlined, HistoryOutlined,
+  SettingOutlined, TableOutlined
+} from '@ant-design/icons';
+import { Button, Radio, Typography, RadioChangeEvent } from 'antd';
+import { createState, Layout, PluginClient, Toolbar, usePlugin, useValue } from 'flipper-plugin';
 import React from 'react';
-import {PluginClient, usePlugin, createState, useValue, Layout } from 'flipper-plugin';
-import { Button } from 'antd';
-
+import {useCallback} from 'react';
 type RealmPluginState = {
   database: Number, 
   objects: Array<Object>,
   schemas: Array<Object>,
+  viewMode: 'data' | 'schemas' | 'RQL'
 }
 
 type Events = {
@@ -37,7 +43,8 @@ export function plugin(client: PluginClient<Events, Methods>) {
   const pluginState = createState<RealmPluginState>({
     database: 0,
     objects: [],
-    schemas: []
+    schemas: [],
+    viewMode: 'data'
   });
 
   client.onMessage("getObjects", (data: ObjectsMessage) => {
@@ -67,7 +74,16 @@ export function plugin(client: PluginClient<Events, Methods>) {
     client.send("getSchemas", undefined);
   }
 
-  return {state: pluginState, getObjects, getSchemas};
+  const updateViewMode = (event: {
+    viewMode: 'data' | 'schemas' | 'RQL';
+  }) => {
+    pluginState.update((state) => {
+      state.viewMode = event.viewMode;
+     // state.error = null;
+    });
+  };
+
+  return {state: pluginState, getObjects, getSchemas, updateViewMode};
 }
 
 // Read more: https://fbflipper.com/docs/tutorial/js-custom#building-a-user-interface-for-the-plugin
@@ -77,20 +93,69 @@ export function Component() {
   const state = useValue(instance.state);
   console.log(state.schemas)
 
+
+  const onViewModeChanged = useCallback(
+    (evt: RadioChangeEvent) => {
+      instance.updateViewMode({viewMode: evt.target.value ?? 'data'});
+    },
+    [instance],
+  );
+
+  const onDataClicked = useCallback(() => {
+    instance.updateViewMode({viewMode: 'data'});
+  }, [instance]);
+
+  const onSchemasClicked = useCallback(() => {
+    instance.updateViewMode({viewMode: 'schemas'});
+  }, [instance]);
+
+  const onRQLClicked = useCallback(() => {
+    instance.updateViewMode({viewMode: 'RQL'});
+  }, [instance]);
+
+
+  console.log(state.viewMode)
+
   return (
-    <Layout.ScrollContainer>
-<Button onClick = {() => instance.getObjects()} title="Get Objects" >button</Button>
-<Button onClick = {() => instance.getSchemas()} title="Get Objects" >get schemas</Button>
-      {state.objects.map((obj) => {
-        // @ts-ignore
-        return (<div key={ obj._id}>{JSON.stringify(obj)}</div>)
-      })}
-      <div>SCHEMAS</div>
-      {state.schemas.map((schema) => {
-        // @ts-ignore
-        return (<div key={ schema.name}>{JSON.stringify(schema)}</div>)
-      })}
-    </Layout.ScrollContainer>
+    <Layout.Container grow>
+      <Toolbar position="top">
+        <Radio.Group value={state.viewMode} onChange={onViewModeChanged}>
+          <Radio.Button value="data" onClick={onDataClicked}>
+            <TableOutlined style={{marginRight: 5}} />
+            <Typography.Text>Data</Typography.Text>
+          </Radio.Button>
+          <Radio.Button onClick={onSchemasClicked} value="schemas">
+            <SettingOutlined style={{marginRight: 5}} />
+            <Typography.Text>Schemas</Typography.Text>
+          </Radio.Button>
+          <Radio.Button onClick={onRQLClicked} value="RQL">
+            <ConsoleSqlOutlined style={{marginRight: 5}} />
+            <Typography.Text>RQL</Typography.Text>
+          </Radio.Button>
+        </Radio.Group>
+      </Toolbar>
+     {state.viewMode === 'data' ? (
+      <div>
+        <Button onClick = {() => instance.getObjects()} title="Get Objects" >button</Button>
+        <Button onClick = {() => instance.getSchemas()} title="Get Objects" >get schemas</Button>
+              {state.objects.map((obj) => {
+                // @ts-ignore
+                return (<div key={ obj._id}>{JSON.stringify(obj)}</div>)
+              })}
+              <div>SCHEMAS</div>
+              {state.schemas.map((schema) => {
+                // @ts-ignore
+                return (<div key={ schema.name}>{JSON.stringify(schema)}</div>)
+              })}
+      </div>
+      ) : null} 
+      {state.viewMode === 'schemas' ?
+            <div>schemas tab</div>
+      : null} 
+      {state.viewMode === 'RQL' ? 
+              <div>RQL tab</div>
+      : null}
+    </Layout.Container>
     
   );
 }
