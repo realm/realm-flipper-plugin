9701a43a676873b21259146f75ca5aa9a0ee23bc
lastnameswayne <swaynedev@gmail.com>
2022-07-14 11:38:38 +0200

    Added new types and extracted row-creation into a seperate function

--- realmFlipperPlugin/flipper-plugin-realm/src/pages/SchemaVisualizer.tsx
+++ realmFlipperPlugin/flipper-plugin-realm/src/pages/SchemaVisualizer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import {Value, renderValue} from './TypeBasedValueRenderer';
-import {SchemaResponseObject} from '../index'
+import {Value, renderValue} from '../utils/TypeBasedValueRenderer';
+import {SchemaResponseObject, SchemaPropertyValue} from '../index'
 import {DataTable, DataTableColumn, Layout, useMemoize} from 'flipper-plugin';
+
 function createColumnConfig(columns: string[]) {
     const columnObjs: DataTableColumn<{[key: string]: Value}>[] = columns.map(
       (c) => ({
@@ -14,44 +15,41 @@ function createColumnConfig(columns: string[]) {
     );
     return columnObjs;
   }
-  function createRows(
-    properties: Object[]
-  ): Object[] {
-    return rows.map((values) =>
-      values.reduce((acc: {[key: string]: Value}, cur: Value, i: number) => {
-        acc[columns[i]] = cur;
-        return acc;
-      }, {}),
-    );
+
+  function createRows(properties: { [key: string]: SchemaPropertyValue; }, primaryKey: String): {[key: string]: Value}[] {
+    let newRows: {[key: string]: Value}[] = []
+    Object.values(properties).forEach((value: SchemaPropertyValue) => {
+      let rowObj: {[key: string]: Value} = {}
+      Object.entries(value).forEach(([key, value]: [string, boolean | string | number]) => {
+          rowObj[key]={type: typeof value, value: value} as Value
+      })
+      rowObj["primaryKey"]={type: "boolean", value: primaryKey===value.name}
+      newRows.push(rowObj)
+     
+  })
+    return newRows;
   }
 export default React.memo((props: {schemas: Array<SchemaResponseObject>}) => {
+    console.log(props)
     const {schemas} = props;
     const [schemaObjects] = schemas
-    const {properties, primaryKey} = schemaObjects
-    let newRows = []
-    Object.entries(properties).forEach(([key, value], index) => {
-        let newObj = {}
-        Object.entries(value).forEach(([key, value]) => {
-            newObj[key]={type: typeof value, value: value}
-        })
-        console.log("value here", value)
-        newObj["primaryKey"]={type: "boolean", value: primaryKey===value.name}
-        newRows.push(newObj)
-       // newRows[index]=
-    })
+    const {name, properties, primaryKey} = schemaObjects
     const columns =  ["name","type", "mapTo","indexed", "optional", "primaryKey"]
     const columnObjs = useMemoize(
         (columns: string[]) => createColumnConfig(columns),
         [columns],
       );
-
-      return (
+    const rows = createRows(properties, primaryKey);
+    
+    
+    return (
        <Layout.Container height={400}>
-      <DataTable<{[key: string]: Value}>
-        records={newRows}
-        columns={columnObjs}
-        enableSearchbar={false}
-      />
+        <Text>{name}</h1>
+        <DataTable<{[key: string]: Value}>
+          records={rows}
+          columns={columnObjs}
+          enableSearchbar={false}
+        />
     </Layout.Container>
     )
 })
