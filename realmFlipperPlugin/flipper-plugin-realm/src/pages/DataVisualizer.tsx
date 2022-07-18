d8907ff4de832f1b54e4242f5276d0253d0bdcbf
Finn Baumann <hello@finnbaumann.com>
2022-07-18 16:06:09 +0200

    Added comments

--- realmFlipperPlugin/flipper-plugin-realm/src/pages/DataVisualizer.tsx
+++ realmFlipperPlugin/flipper-plugin-realm/src/pages/DataVisualizer.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useState } from "react";
-import { Layout, DataTableColumn, DataTable, useMemoize } from "flipper-plugin";
+import { Layout, DataTable, useMemoize } from "flipper-plugin";
 import { Radio } from "antd";
 import Prettyjson from "../components/Prettyjson";
-import { renderValue, Value } from "../utils/TypeBasedValueRenderer";
+import { Value } from "../utils/TypeBasedValueRenderer";
 import { SchemaResponseObject } from "../index";
 import { createColumnConfig } from "../pages/SchemaVisualizer";
 
@@ -12,11 +12,10 @@ export default function DataVisualizer(props: {
   schemas: Array<SchemaResponseObject>;
   getObjects: Function;
 }) {
-  //var objectview = false;
-  var objects = props.objects;
+  // State to switch between views. true = objectView, false = tableView
+  const [objectView, setView] = useState(true);
 
-  const [objectview, setView] = useState(true);
-
+  // Return buttons + objectView or tableView
   return (
     <Layout.ScrollContainer>
       <Layout.Container>
@@ -24,52 +23,57 @@ export default function DataVisualizer(props: {
           <Radio.Button onClick={() => setView(true)}>Object View</Radio.Button>
           <Radio.Button onClick={() => setView(false)}>Table View</Radio.Button>
         </Radio.Group>
-        <Radio.Group>
-          <Radio.Button onClick={() => props.getObjects()}>
-            Refresh
-          </Radio.Button>
-        </Radio.Group>
       </Layout.Container>
       <Layout.Container>
-        {objectview ? <ObjectView /> : <TableView />}
+        {objectView ? <ObjectView /> : <TableView />}
       </Layout.Container>
     </Layout.ScrollContainer>
   );
 
+  // Render objectView
   function ObjectView() {
-    return objects.map((obj) => {
-      return (
-        <Prettyjson key={obj._id} json={obj}>
-          {" "}
-        </Prettyjson>
-      );
-    });
+    // Map over all objects and genereate a Prettyjson component for each.
+    return (
+      <Layout.Container>
+        {props.objects.map((obj) => {
+          return (
+            //@ts-ignore
+            <Prettyjson key={obj._id} json={obj}>
+              {" "}
+            </Prettyjson>
+          );
+        })}
+      </Layout.Container>
+    );
   }
 
+  // Render TableView
   function TableView() {
+    // Take the keys of the schema in position 0 and put them into an array.
     const columns = Object.keys(props.schemas[0].properties).map((x) => {
       return x;
     });
 
-    const columnObjs = useMemoize(
+    // Genereate a list of column objects.
+    const columnObjs: Array<Object> = useMemoize(
       (columns: string[]) => createColumnConfig(columns),
       [columns]
     );
 
+    // Instantiate an array with row objects and fill it with data.
     const rows: Array<Object> = [];
-
-    objects.forEach(function (obj) {
-      var rowObj: Object = {};
+    props.objects.forEach(function (obj: Object) {
+      let rowObj: { [key: string]: Value } = {};
       columns.forEach(function (key) {
         rowObj[key] = { type: typeof obj[key], value: obj[key] };
       });
       rows.push(rowObj);
     });
 
+    // Render a data table and return it.
     return (
-      <Layout.Container height={400}>
+      <Layout.Container height={800}>
         <DataTable<{ [key: string]: Value }>
-          data-testid={name}
           records={rows}
           columns={columnObjs}
           enableSearchbar={false}
