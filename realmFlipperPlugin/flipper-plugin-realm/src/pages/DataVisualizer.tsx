67f7ef403943a50032160ac7903c8da2622c72a2
Finn Baumann <hello@finnbaumann.com>
2022-07-20 15:32:12 +0200

    Co-authored-by: Tomasz Marzec - Changed Table to antd

--- realmFlipperPlugin/flipper-plugin-realm/src/pages/DataVisualizer.tsx
+++ realmFlipperPlugin/flipper-plugin-realm/src/pages/DataVisualizer.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import { Layout, DataTable, DataTableColumn, useMemoize } from "flipper-plugin";
-import { Radio } from "antd";
+import { Radio, Table } from "antd";
 import Prettyjson from "../components/Prettyjson";
 import { Value, renderValue } from "../utils/TypeBasedValueRenderer";
 import { SchemaResponseObject } from "../index";
-import { createColumnConfig } from "../pages/SchemaVisualizer";
-import SchemaSelect from "../components/SchemaSelect";
 
 export default function DataVisualizer(props: {
   objects: Array<Object>;
@@ -53,59 +51,49 @@ export default function DataVisualizer(props: {
     }
   }
 
-  // Render TableView
   function TableView() {
-    // Find the selected schema in the schemas array. Take the keys of the schema and put them into an array.
     const currentSchema = props.schemas.find(
       (schema) => schema.name === props.selectedSchema
     );
 
-    if (currentSchema != undefined) {
-      // Genereate a list of column objects.
-      const columnObjs = useMemoize(
-        (currentSchema: SchemaResponseObject) =>
-          createColumnConfigFromSchema(currentSchema),
-        [currentSchema]
-      );
-
-      // Instantiate an array with row objects and fill it with data.
-      const rows: Array<Object> = [];
-      props.objects.forEach(function (obj: Object) {
-        let rowObj: { [key: string]: Value } = {};
-        columnObjs.forEach(function (c) {
-          rowObj[c.key] = { type: typeof obj[c.key], value: obj[c.key] };
-        });
-        rows.push(rowObj);
-      });
-
-      // Render a data table and return it.
-      return (
-        <Layout.Container height={800}>
-          <DataTable<{ [key: string]: Value }>
-            records={rows}
-            columns={columnObjs}
-            enableSearchbar={false}
-          />
-        </Layout.Container>
-      );
-    } else {
+    if (currentSchema === undefined) {
       return <Layout.Container>Please select schema.</Layout.Container>;
     }
 
-    
-  }
-}
+    const columnObjs = Object.keys(currentSchema.properties).map((propName) => {
+      const property = currentSchema.properties[propName];
+      return {
+        title: property.name + " [" + property.type + "]",
+        key: property.name,
+        dataIndex: property.name,
+        sorter: (a, b) => {
+          if (a[propName] > b[propName]) {
+            return 1;
+          }
+          else if (a[propName] < b[propName]) {
+            return -1;
+          }
+          else {
+            return 0;
+          }
+        },
+        onFilter: (value: string, record: any) => record[propName].startsWith(value),
+        filterSearch: true,
 
-function createColumnConfigFromSchema(
-  schema: SchemaResponseObject
-): Array<DataTableColumn> {
-  const columnObjs: DataTableColumn<{ [key: string]: Value }>[] =
-    Object.keys(schema.properties).map((c) => ({
-      key: c,
-      title: c + " [" + schema.properties[c].type + "]",
-      onRender(row) {
-        return renderValue(row[c]);
-      },
-    }));
-  return columnObjs;
+      };
+    });
+
+    const rowObjs = props.objects.map((obj, id) => {
+      return {
+        ...obj,
+        key: id
+      }
+    })
+
+    return (
+      <Layout.Container height={800}>
+      <Table dataSource={rowObjs} columns={columnObjs} />;
+      </Layout.Container>
+    )
+  }
 }
