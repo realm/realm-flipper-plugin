c56f98ce53a3e9e7af575578048a6233b916f5b9
Finn Baumann <hello@finnbaumann.com>
2022-07-21 15:14:29 +0200

    UI improvements on table.

--- realmFlipperPlugin/flipper-plugin-realm/src/pages/DataVisualizer.tsx
+++ realmFlipperPlugin/flipper-plugin-realm/src/pages/DataVisualizer.tsx
@@ -1,15 +1,8 @@
 import React from "react";
-import { Layout} from "flipper-plugin";
+import { Layout } from "flipper-plugin";
 import { Radio, Table, Tooltip } from "antd";
-import Prettyjson from "../components/Prettyjson";
 import { SchemaResponseObject } from "../index";
-<<<<<<< HEAD
-import { createColumnConfig } from "../pages/SchemaVisualizer";
 import ObjectAdder from "../components/ObjectAdder";
-import SchemaSelect from "../components/SchemaSelect";
-=======
-import ObjectAdder from "../components/ObjectAdder";
->>>>>>> bf88fd7 (ellipsis in data table)
 
 export default function DataVisualizer(props: {
   objects: Array<Object>;
@@ -18,34 +11,21 @@ export default function DataVisualizer(props: {
   selectedSchema: String;
   addObject: Function;
 }) {
-
-  const getCurrentSchema = () => {
-    return props.schemas.find(schema => schema.name === props.selectedSchema);
-  }
-  // State to switch between views. true = objectView, false = tableView
-  const [objectView, setView] = useState(true);
   const getCurrentSchema = () => {
     return props.schemas.find((schema) => schema.name === props.selectedSchema);
   };
-
 
   // Return buttons + objectView or tableView
   return (
     <Layout.ScrollContainer>
       <Layout.Container>
         <Radio.Group>
-          <Radio.Button onClick={() => setView(true)}>Object View</Radio.Button>
-          <Radio.Button onClick={() => setView(false)}>Table View</Radio.Button>
-<<<<<<< HEAD
-          {<ObjectAdder schema={getCurrentSchema()} addObject={props.addObject}/>}
-=======
           {
             <ObjectAdder
               schema={getCurrentSchema()}
               addObject={props.addObject}
             />
           }
->>>>>>> bf88fd7 (ellipsis in data table)
         </Radio.Group>
       </Layout.Container>
       <Layout.Container>
@@ -53,27 +33,6 @@ export default function DataVisualizer(props: {
       </Layout.Container>
     </Layout.ScrollContainer>
   );
-
-  // Render objectView
-  function ObjectView() {
-    if (props.selectedSchema !== "") {
-      // Map over all objects and genereate a Prettyjson component for each.
-      return (
-        <Layout.Container>
-          {props.objects.map((obj) => {
-            return (
-              //@ts-ignore
-              <Prettyjson key={obj._id} json={obj}>
-                {" "}
-              </Prettyjson>
-            );
-          })}
-        </Layout.Container>
-      );
-    } else {
-      return <Layout.Container>Please select schema.</Layout.Container>;
-    }
-  }
 
   function TableView() {
     const currentSchema = props.schemas.find(
@@ -86,30 +45,40 @@ export default function DataVisualizer(props: {
 
     const columnObjs = Object.keys(currentSchema.properties).map((propName) => {
       const property = currentSchema.properties[propName];
+
       return {
-        title: property.name + " [" + property.type + "]",
+        title: property.optional
+          ? property.name + " [" + property.type + "?]"
+          : property.name + " [" + property.type + "]",
         key: property.name,
         dataIndex: property.name,
-        width: 150,     
+        width: 150,
         ellipsis: {
           showTitle: false,
         },
 
         render: (text) => {
-          console.log("Tooltip: " + text);
-          console.log(typeof text === "object" ? JSON.stringify(text) : text);
-
           return (
             <Tooltip
               placement="topLeft"
-              title={JSON.stringify(text)}
+              title={
+                typeof text === "object"
+                  ? JSON.stringify(text)
+                  : typeof text === "boolean"
+                  ? text.toString()
+                  : text
+              }
               key={Math.floor(Math.random() * 10000000)}
             >
-              {typeof text === "object" ? JSON.stringify(text) : text.toString()}
+              {typeof text === "object"
+                ? JSON.stringify(text)
+                : typeof text === "boolean"
+                ? text.toString()
+                : text}
             </Tooltip>
           );
         },
-        sorter: (a, b) => {
+        sorter: (a: any, b: any) => {
           if (a[propName] > b[propName]) {
             return 1;
           } else if (a[propName] < b[propName]) {
@@ -136,7 +105,13 @@ export default function DataVisualizer(props: {
         <Table
           dataSource={rowObjs}
           columns={columnObjs}
-          pagination={{ position: ["topLeft", "bottomLeft"] }}
+          pagination={{
+            position: ["topLeft", "bottomLeft"],
+            defaultPageSize: 20,
+            showSizeChanger: true,
+            pageSizeOptions: ["10", "20", "30", "50", "100", "500"],
+          }}
+          size="small"
         />
       </Layout.Container>
     );
