9f698ac25cc8b7d53c79edc29d289dcee6fb923f
lastnameswayne <swaynedev@gmail.com>
2022-09-01 14:38:24 +0200

    Renamed schemas graph => graph of schemas

--- realmFlipperPlugin/flipper-plugin-realm/src/components/common/ViewModeTabs.tsx
+++ realmFlipperPlugin/flipper-plugin-realm/src/components/common/ViewModeTabs.tsx
@@ -13,27 +13,30 @@ const ViewModeTabs = (props: {
   viewMode: 'data' | 'schemas' | 'RQL' | 'schemaGraph';
 }) => {
   return (
-      <Radio.Group value={props.viewMode}>
-        <Radio.Button value="data" onClick={() => props.setViewMode('data')}>
-          <TableOutlined style={{ marginRight: 5 }} />
-          <Typography.Text>Data</Typography.Text>
-        </Radio.Button>
-        <Radio.Button
-          onClick={() => props.setViewMode('schemas')}
-          value="schemas"
-        >
-          <SettingOutlined style={{ marginRight: 5 }} />
-          <Typography.Text>Schema</Typography.Text>
-        </Radio.Button>
-        {/* <Radio.Button onClick={() => props.setViewMode('RQL')} value="RQL">
+    <Radio.Group value={props.viewMode}>
+      <Radio.Button value="data" onClick={() => props.setViewMode('data')}>
+        <TableOutlined style={{ marginRight: 5 }} />
+        <Typography.Text>Data</Typography.Text>
+      </Radio.Button>
+      <Radio.Button
+        onClick={() => props.setViewMode('schemas')}
+        value="schemas"
+      >
+        <SettingOutlined style={{ marginRight: 5 }} />
+        <Typography.Text>Schema</Typography.Text>
+      </Radio.Button>
+      {/* <Radio.Button onClick={() => props.setViewMode('RQL')} value="RQL">
           <ConsoleSqlOutlined style={{ marginRight: 5 }} />
           <Typography.Text>RQL</Typography.Text>
         </Radio.Button> */}
-        <Radio.Button onClick={() => props.setViewMode('schemaGraph')} value="schemaGraph">
-        <RadarChartOutlined style={{ marginRight: 5 }}/>
-          <Typography.Text>Schemas graph</Typography.Text>
-        </Radio.Button>
-      </Radio.Group>
+      <Radio.Button
+        onClick={() => props.setViewMode('schemaGraph')}
+        value="schemaGraph"
+      >
+        <RadarChartOutlined style={{ marginRight: 5 }} />
+        <Typography.Text>Graph of schemas</Typography.Text>
+      </Radio.Button>
+    </Radio.Group>
   );
 };
 
