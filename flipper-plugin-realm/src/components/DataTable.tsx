93b4460d629e6f2951831af3f552d0957c950394
Finn Baumann <hello@finnbaumann.com>
2022-09-07 15:05:12 +0200

    Commented out tableLayout='auto'

--- flipper-plugin-realm/src/components/DataTable.tsx
+++ flipper-plugin-realm/src/components/DataTable.tsx
@@ -383,7 +383,7 @@ PropertyType) => {
           onChange={handleOnChange}
           pagination={false}
           scroll={{ scrollToFirstRowOnChange: false }}
-          tableLayout="auto"
+          // tableLayout="auto"
         />
       </InfiniteScroll>
     </div>
