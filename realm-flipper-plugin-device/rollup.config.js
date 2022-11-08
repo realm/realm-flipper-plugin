a1d1c404f958c60d4fef85a1ac9810108406c016
Siarhei Haikou <9068073+haikov@users.noreply.github.com>
2022-11-08 16:53:43 +0200

    Fix react npm dependency & rollup config (#109)
    * set required react version to latest
    * sync package-lock.json
    * fix rollup build command
    * set strict react dependency
    
    Co-authored-by: Kenneth Geisshirt <kenneth.geisshirt@mongodb.com>

--- realm-flipper-plugin-device/rollup.config.js
+++ realm-flipper-plugin-device/rollup.config.js
@@ -2,7 +2,9 @@ import typescript from "rollup-plugin-typescript";
 
 export default {
   input: "./src/RealmPlugin.tsx",
-  output: "./dist/index.js",
+  output: {
+    file: "./dist/index.js",
+  },
   plugins: [typescript()],
-  external: ["react", "react-dom", "react-native-flipper", "realm"]
+  external: ["react", "react-dom", "react-native-flipper", "realm"],
 };
